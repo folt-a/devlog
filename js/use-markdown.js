8459db1c02ed8cc0521093b9b5075e3c153864c2
folt-a <folt-a@users.noreply.github.com>
2020-12-23 09:37:53 +0000

    deploy: 7caacde57322f36861b7c3ebc4752a7dc7892eeb

---
+++ js/use-markdown.js
@@ -0,0 +1,41 @@
+(async function () {
+    const result = await fetchAPI('./articles/testa.md', null, 'get');
+
+    document.getElementById('article1').innerHTML =
+        marked(result);
+})();
+
+
+async function fetchAPI(url, formData, method = 'post') {
+    // 4xx系, 5xx系のエラーをさばくハンドラ
+    var handleErrors = function (response) {
+
+        // 404ならnullを返す
+        if (response.status === 404) {
+            return null;
+        }
+        // 4xx系, 5xx系エラーのときには response.ok==false になる
+        else if (!response.ok) {
+            return response.json().then(function (err) {
+                throw new ApplicationError(err.code, err.message);
+            });
+        } else {
+            return response;
+        }
+    }
+
+    return await fetch(url, {
+            method: method.toUpperCase(),
+            body: formData
+        })
+        .then(handleErrors)
+        .then(response => {
+            if (response == null) {
+                return null;
+            }
+            return response.text();
+        })
+        .catch(error => {
+            return error;
+        });
+}
