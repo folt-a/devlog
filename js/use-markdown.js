e57fcdc528be6af1500611d325a660ba386355bd
folt-a <faultun@gmail.com>
2021-01-01 12:31:26 +0900

    tailwindcss

--- js/use-markdown.js
+++ js/use-markdown.js
@@ -1,24 +1,29 @@
 (async function () {
     // config load
-    const config = JSON.parse(await fetchAPI('./js/article-config.json', null, 'get'))
-    config.titles
+    const config = JSON.parse(await fetchAPI('./js/article-config.json', null, 'get'));
 
-    const articleElms = document.querySelectorAll('[data-article]')
+    const articleElms = document.querySelectorAll('article');
 
     // 記事要素にMarkdown記事を流し込む
     let elmIndex = 0;
     for (let articleIndex = 0; elmIndex < config.topArticleCounts; articleIndex++) {
-        const article = config.articles[articleIndex]
+        const article = config.articles[articleIndex];
+        const publishedDate = dayjs(article.publishedDate, 'YYYYMMDD');
 
         //非公開記事、予約投稿記事は表示しない。
-        if (article.isPublished && dayjs(article.publishedDate, 'YYYYMMDD') <= dayjs()) {
-            console.log(articleIndex)
-            console.log(elmIndex)
-            console.log(article)
-            articleElms[elmIndex].innerHTML = marked(await getArticleBody(article))
-            ++elmIndex
+        if (article.isPublished && publishedDate <= dayjs()) {
+            // 本文
+            articleElms[elmIndex].querySelector('[data-article]').innerHTML = marked(await getArticleBody(article));
+
+            // タイトル
+            articleElms[elmIndex].querySelector('[data-title]').innerHTML = article.title;
+
+            // 公開日時
+            articleElms[elmIndex].querySelector('[data-published-date]').innerHTML = publishedDate.format('YYYY年MM月DD日');
+
+            ++elmIndex;
         } else {
-            ++articleIndex
+            ++articleIndex;
         }
     }
 })()
@@ -30,11 +35,11 @@
  * @param {Object} article 記事オブジェクト (config.articles)
  */
 async function getArticleBody(article) {
-    let result = await fetchAPI('./articles/' + article.title + '.md?' + article.updatedDate, null, 'get')
-    result = result.replace(/---/i, "")
-    const indexs = result.indexOf('---')
-    const honbun = result.substr(indexs + 4, result.length)
-    return honbun
+    let result = await fetchAPI('./articles/' + article.title + '.md?' + article.updatedDate, null, 'get');
+    result = result.replace(/---/i, "");
+    const indexs = result.indexOf('---');
+    const honbun = result.substr(indexs + 4, result.length);
+    return honbun;
 }
 
 // API
@@ -44,15 +49,15 @@ async function fetchAPI(url, formData, method = 'post') {
 
         // 404ならnullを返す
         if (response.status === 404) {
-            return null
+            return null;
         }
         // 4xx系, 5xx系エラーのときには response.ok==false になる
         else if (!response.ok) {
             return response.json().then(function (err) {
-                throw new ApplicationError(err.code, err.message)
+                throw new ApplicationError(err.code, err.message);
             })
         } else {
-            return response
+            return response;
         }
     }
 
@@ -63,11 +68,11 @@ async function fetchAPI(url, formData, method = 'post') {
         .then(handleErrors)
         .then(response => {
             if (response == null) {
-                return null
+                return null;
             }
-            return response.text()
+            return response.text();
         })
         .catch(error => {
-            return error
+            return error;
         })
 }
